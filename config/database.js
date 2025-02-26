76a46d7e20bdf4213cc88a4331b9d0858ec9010e
Ankur-dev <itzankurhere@gmail.com>
2025-02-26 18:07:54 +0530

    Refine Azure PostgreSQL database connection settings

--- config/database.js
+++ config/database.js
@@ -13,9 +13,10 @@ module.exports = ({ env }) => {
           database: env('DATABASE_NAME'),
           user: env('DATABASE_USERNAME'),
           password: env('DATABASE_PASSWORD'),
-          ssl: env.bool('DATABASE_SSL', false)
-            ? { rejectUnauthorized: false }
-            : false,
+          ssl: {
+            rejectUnauthorized: false,
+            ca: env('DATABASE_CA', undefined),
+          },
         },
         debug: false,
       },
